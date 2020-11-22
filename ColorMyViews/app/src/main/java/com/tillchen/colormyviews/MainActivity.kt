9ea13b6513ec41bb96243bad94ad4f59ecdb32cc
Tianyao Chen <tillchen417@gmail.com>
2020-11-22 22:44:40 +0100

    Finish 2.3

--- ColorMyViews/app/src/main/java/com/tillchen/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/tillchen/colormyviews/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tillchen.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -32,15 +36,19 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
 
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
-            item.setOnClickListener {makeColored(it)}
+            item.setOnClickListener { makeColored(it) }
         }
     }
 }
